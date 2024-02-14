4b112be21b89a2bf5d637809853f190bd224deea
otherworldly94b <and.charalambous@gmail.com>
2024-02-14 11:08:29 +0000

    chaange config vars in clear

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f0qq&(*&kz4^-!ii1fuuxoi)=ul-nx&2z*&k-v_rg%e@t_)rv2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
