1ad31fe1eae7e740c8adad217c1b7735a21a0e18
otherworldly94b <and.charalambous@gmail.com>
2024-02-16 22:59:08 +0000

    modify the standard admin da shboard using both Django methods and integrate the Summernote package.

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'anime_blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- codestar/urls.py
+++ codestar/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("anime_blog.urls"), name="anime-blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("anime_blog.urls"), name="anime-blog-urls"),
 ]
